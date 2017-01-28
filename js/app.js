f7c3552b3f5f379e32fd590cfcdad9aadc30814d
Lithesh <lithesh.nash.com>
2017-01-28 20:13:55 +0530

    Initialize check

---
+++ js/app.js
@@ -0,0 +1,23 @@
+//var app = angular.module("myApp", ["ngRoute","ui.bootstrap"]);
+var app = angular.module("myApp", ["ui.bootstrap"]);
+/*app.  config(['$locationProvider', '$routeProvider',
+    function config($locationProvider, $routeProvider) {
+        $locationProvider.html5Mode(true);
+        $locationProvider.hashPrefix('!');
+        $routeProvider
+        .when("/", {
+            templateUrl: "../views/home.html"
+        })
+        .when("/aboutus", {
+            templateUrl: "../views/aboutus.html"
+        })
+        .when("/contactus", {
+            templateUrl : "../views/contact.html"
+        })
+        .when("/products", {
+            templateUrl: "../views/products.html"
+        })
+        // .otherwise({ redirectTo: '/home' });
+        .otherwise({  template: "Page Not found" });
+        }
+  ]);*/
