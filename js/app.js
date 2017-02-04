003751cc3fd4281788e5546d5e489d1c3cec2021
Lithesh <lithesh.nash@gmail.com>
2017-02-04 19:57:47 +0530

    routing check 1

--- js/app.js
+++ js/app.js
@@ -1,22 +1,38 @@
-var app = angular.module("myApp", ["ngRoute","ui.bootstrap"]);
+var app = angular.module("myApp", ["ngRoute","ngAnimate"]);
 app.  config(['$locationProvider', '$routeProvider',
     function config($locationProvider, $routeProvider) {
         $locationProvider.html5Mode(true);
         $locationProvider.hashPrefix('!');
         $routeProvider
         .when("/", {
-            templateUrl: "../views/home.html"
+            templateUrl: "../views/home.html",
+            controller: 'mainController'
         })
         .when("/aboutus", {
-            templateUrl: "../views/aboutus.html"
+            templateUrl: "../views/aboutus.html",
+            controller: 'mainController'
         })
         .when("/contactus", {
-            templateUrl : "../views/contact.html"
+            templateUrl : "../views/contact.html",
+            controller: 'mainController'
         })
-        .when("/products", {
-            templateUrl: "../views/products.html"
+        .when("/services", {
+            templateUrl: "../views/services.html",
+            controller: 'mainController'
         })
         // .otherwise({ redirectTo: '/home' });
         .otherwise({  template: "Page Not found" });
         }
   ]);
+
+app.controller('mainController', function($scope) {
+    $scope.pageClass = 'page-home';
+});
+
+app.controller('aboutController', function($scope) {
+    $scope.pageClass = 'page-about';
+});
+
+app.controller('contactController', function($scope) {
+    $scope.pageClass = 'page-contact';
+});
